971642d94bc440218c82f75bb4958c84221ddbca
Emmanuel Di Nicola <di_nicola_emmanuel@hotmail.com>
2023-09-06 22:11:00 +0200

    fin module 3

--- moncode/mysite/polls/views.py
+++ moncode/mysite/polls/views.py
@@ -32,3 +32,40 @@ def index(request):
         "latest_question_list": latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+    
+from django.shortcuts import render
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+#les erreurs 404
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
+
+
+# ...
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
