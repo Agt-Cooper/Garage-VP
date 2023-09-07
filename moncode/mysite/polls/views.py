dbbe048944a03329b604c30e437e62079b64483e
Emmanuel Di Nicola <di_nicola_emmanuel@hotmail.com>
2023-09-07 12:40:10 +0200

    vraie correction

--- moncode/mysite/polls/views.py
+++ moncode/mysite/polls/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
-from django.http import Http404
-from django.template import get_object_or_404, render
 
 
 from .models import Question
